52459dc2a84848b15d642b048ff78344a56241f0
tygrski <77369211+tygrski@users.noreply.github.com>
2021-10-04 19:31:26 -0500

    run error

--- _tests_/random.test.js
+++
@@ -1,6 +0,0 @@
-const { TestWatcher } = require('@jest/core');
-const checkIfEqual = require('../lib/random.js');
-
-TestWatcher('checks if 10 is equal to 10', () => {
-  expect(checkIfEqual(10,10)).toBe(true);
-});
